745f88c78ce0c4c3de3df3cbfe46214c33b621f0
Unclezs <1585503310@qq.com>
2020-12-11 18:56:17 +0800

    [update]更新发布脚本

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,3 @@
 @echo off
 #编译 及 更新到云端 app打开后可以获取到更新
-mvn clean package && mvn exec:exec@deploy-app
+mvn clean package -P release,windows -U -X -DskipTests && mvn exec:exec@deploy-app -P release,windows
