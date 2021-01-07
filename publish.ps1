fea53e9fae12f52e6be8206dc6c67a8fc74efab5
unclezs <1585503310@qq.com>
2021-01-07 08:02:14 +0800

    :wrench: 修改windows打包脚本

--- publish.ps1
+++ publish.ps1
@@ -1,2 +1,3 @@
 #编译 及 更新到云端 app打开后可以获取到更新
-mvn clean package -P release,windows -U -X -DskipTests && mvn exec:exec@deploy-app -P release,windows
+mvn clean package -P release,windows -U -X -DskipTests
+mvn exec:exec@deploy-app -P release,windows
